b042425e0adbcdbe2d35d0a9f57eb4f385ac8922
ishwo6375 <ishaan.prazapati6375@gmail.com>
2022-01-01 10:51:56 -0500

    added js for mobile menu

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
+const menuBtn = document.querySelector('#menu-btn');
+const closeBtn = document.querySelector('#close-btn');
+const menu = document.querySelector('nav .container ul');
+
+menuBtn.addEventListener('click', () => {
+    menu.style.display = 'block';
+    menuBtn.style.display = 'none';
+    closeBtn.style.display = 'inline-block';
+})
+
+closeBtn.addEventListener('click', () =>{
+     menu.style.display = 'none';
+    menuBtn.style.display = 'inline-block';
+    closeBtn.style.display = 'none';
+})
+
+
+
 window.addEventListener('load', () => {
     let progressBars = document.querySelectorAll('.progress-bar');
 
